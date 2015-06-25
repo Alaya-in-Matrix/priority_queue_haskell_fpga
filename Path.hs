38baa2294ddba2a0e0dd18a17b2c9f6cb14b4726
alaya <lvwenlong_lambda@qq.com>
2015-06-25 20:10:35 +0800

    waiting to init

--- Path.hs
+++ Path.hs
@@ -4,6 +4,13 @@ import CLaSH.Prelude
 
 topEntity = undefined
 
+data Node = Node{
+    idx        :: Int,
+    pred       :: Int,
+    g          :: Int,
+    h          :: Int,
+    isObstacle :: Bool
+} deriving(Show)
 
 data PathFinderState 
     = PWaiting 
@@ -13,19 +20,28 @@ data PathFinderState
     | PPushNeighbor
     | PFinished
     | PError deriving(Show)
-
+-- 由PFinished 转到 PWaiting 应该清空PQueue
+-- 或者在init段清空Queue
 data Info = In{
     xMax  :: Int,
     yMax  :: Int,
     start :: Int,
     end   :: Int
 } deriving(Show)
-data PathOutput
-type PathInput = (Maybe Info)
+
+type PathInput a = (Q.Output a, Maybe Info)
 data PathState = PS {
+    info       :: Info,
     innerState :: PathFinderState
 } deriving(Show)
 
-aStarCtrl :: PathState -> PathInput -> PathState
-aStarCtrl (PS PError)   _        = PS PError
-aStarCtrl (PS PWaiting) Nothing  = PS PWaiting 
+aStarCtrl :: (Ord a) 
+          => PathState 
+          -> PathInput a
+          -> PathState
+aStarCtrl st@(PS info PError)   cin@_                                      = st
+aStarCtrl st@(PS info PWaiting) cin@((Q.Out (Left _) _),_)                 = PS info PError
+aStarCtrl st@(PS info PWaiting) cin@((Q.Out (Right Q.Ready)   _),Nothing)  = st               -- nothing todo
+aStarCtrl st@(PS info PWaiting) cin@((Q.Out (Right Q.Ready)   _),(Just i)) = PS i PInit       -- Waiting -> Init
+aStarCtrl st@(PS info PInit)    cin@((Q.Out (Right Q.Pushing) _),_)        = st
+aStarCtrl st@(PS info PInit)    cin@((Q.Out (Right Q.Poping)  _),_)        = PS info PError
