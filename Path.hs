f2dd6bbe9343041eba51a68328d470400d1abd48
alaya <lvwenlong_lambda@qq.com>
2015-06-26 18:22:04 +0800

    unknown error handler

--- Path.hs
+++ Path.hs
@@ -16,26 +16,19 @@ topEntity = undefined
 aStarCtrl :: PathState                      -- input state
           -> PathInput Node              
           -> PathState
--- error state, report error
 aStarCtrl st@(PS mdc info (PError err) u v _) cin@_ = st
--- error in queue, new state: error, output: report error
-aStarCtrl st@(PS mdc info _        u v upC)         cin@((Q.Out (Left Q.Empty)    _), _,        _)   = PS mdc     info (PError NoPath)    u  v upC
-aStarCtrl st@(PS mdc info _        u v upC)         cin@((Q.Out (Left Q.Overflow) _), _,        _)   = PS mdc     info (PError QOverflow) u  v upC
-aStarCtrl st@(PS mdc info PWaiting u v upC)         cin@((Q.Out (Right Q.Ready)   _), Nothing,  _)   = st                              -- nothing todo
-aStarCtrl st@(PS mdc info PWaiting u v upC)         cin@((Q.Out (Right Q.Ready)   _), (Just i), _)   = PS mdc     info PInitMem           u  v upC        -- Waiting -> Init
-aStarCtrl st@(PS mdc info PWaiting u v upC)         cin@((Q.Out (Right _)         _), _,        _)   = PS mdc     info (PError Unknown)   u  v upC
-aStarCtrl st@(PS 0   info PInitMem u v upC)         cin@((Q.Out (Right Q.Ready)   _), _,        rv)  = PS 0       info PInitQ             rv v upC           -- finished reading mem
-aStarCtrl st@(PS mdc info PInitMem u v upC)         cin@((Q.Out (Right Q.Ready)   _), _,        _)   = PS (mdc-1) info PInitMem           u  v upC    -- reading mem
-aStarCtrl st@(PS mdc info PInitMem u v upC)         cin@((Q.Out (Right _)         _), _,        _)   = PS (mdc-1) info (PError Unknown)   u  v upC    -- reading mem
-aStarCtrl st@(PS mdc info PInitQ   u v upC)         cin@((Q.Out (Right Q.Ready)   _), _,        _)   = PS mdc     info PCheck             u  v upC
-aStarCtrl st@(PS mdc info PCheck   u v upC)         cin@((Q.Out (Right Q.Popping) _), _,        _)   = PS mdc     info (PError Unknown)   u  v upC
-aStarCtrl st@(PS mdc info PCheck   u v upC)         cin@((Q.Out (Right Q.Pushing) _), _,        _)   = st
-aStarCtrl st@(PS mdc info PCheck   u v upC)         cin@((Q.Out (Right Q.Ready)   Nothing),    _, _) = PS mdc     info (PError Unknown)   u  v upC
-aStarCtrl st@(PS mdc info PCheck   u v upC)         cin@((Q.Out (Right Q.Ready)   (Just top)), _, _)
+aStarCtrl st@(PS mdc info _        u v upC)   cin@((Q.Out (Left Q.Empty)    _), _,        _)   = PS mdc     info (PError NoPath)    u  v upC
+aStarCtrl st@(PS mdc info _        u v upC)   cin@((Q.Out (Left Q.Overflow) _), _,        _)   = PS mdc     info (PError QOverflow) u  v upC
+aStarCtrl st@(PS mdc info PWaiting u v upC)   cin@((Q.Out (Right Q.Ready)   _), Nothing,  _)   = st                              -- nothing todo
+aStarCtrl st@(PS mdc info PWaiting u v upC)   cin@((Q.Out (Right Q.Ready)   _), (Just i), _)   = PS 1       info PInitMem           u  v upC        -- Waiting -> Init
+aStarCtrl st@(PS 0   info PInitMem u v upC)   cin@((Q.Out (Right Q.Ready)   _), _,        rv)  = PS 0       info PInitQ             rv v upC           -- finished reading mem
+aStarCtrl st@(PS mdc info PInitMem u v upC)   cin@((Q.Out (Right Q.Ready)   _), _,        _)   = PS (mdc-1) info PInitMem           u  v upC    -- reading mem
+aStarCtrl st@(PS mdc info PInitQ   u v upC)   cin@((Q.Out (Right Q.Ready)   _), _,        _)   = PS mdc     info PCheck             u  v upC
+aStarCtrl st@(PS mdc info PCheck   u v upC)   cin@((Q.Out (Right Q.Pushing) _), _,        _)   = st
+aStarCtrl st@(PS mdc info PCheck   u v upC)   cin@((Q.Out (Right Q.Ready)   (Just top)), _, _)
     | idx top == end info = PS mdc info PFinished        top v upC
     | otherwise           = PS 5   info PProcessNeighbor top v upC    -- read 4 neighbors, with 1 clock delay
 aStarCtrl st@(PS 5   info PProcessNeighbor u v upC) cin@_                                            = PS 4    info PProcessNeighbor u v upC
-aStarCtrl st@(PS 0   info PProcessNeighbor u v upC) cin@((Q.Out (Right Q.Pushing) _),_,_)            = PS 0    info (PError Unknown) u v upC-- finished reading memory, check whether poping is finished
 aStarCtrl st@(PS 0   info PProcessNeighbor u v upC) cin@((Q.Out (Right Q.Popping) _),_,_)            = st   -- stall
 aStarCtrl st@(PS 0   info PProcessNeighbor u v upC) cin@((Q.Out (Right Q.Ready)   _),_,_)            = PS 1    info PPushNeighbor u v 0
 aStarCtrl st@(PS mdc info PProcessNeighbor u v upC) cin@((Q.Out (Right _)         _),_,rv)           = PS mdc' info PProcessNeighbor u v' upC
@@ -51,8 +44,12 @@ aStarCtrl st@(PS mdc info PPushNeighbor u v upC) cin@_
     | otherwise = PS 1 info PPushNeighbor u v (upC + 1)
       where waiting = let qStatus ((Q.Out s _),_,_) = s
                        in (qStatus cin) /= Right Q.Ready || mdc > 0
-aStarCtrl st@(PS mdc info PFinished u v upC) _ = PS 0 info PWaiting u v 0
+aStarCtrl st@(PS mdc info PFinished u v upC) cin@_ = PS 0 info PWaiting u v 0
+aStarCtrl st@_                               cin@_ = errSt st     -- default situation, error state
+    where errSt (PS mdc info _ u v upC) = PS mdc info (PError Unknown) u v upC
+type Addr = (Unsigned 4)
 type PathInput a = (Q.Output a, Maybe Info, Node)
+type PathOut a = (Q.Input a, Addr)
 data PathState   = PS {
     memDelayCounter::(Unsigned 4),
     info :: Info, 
